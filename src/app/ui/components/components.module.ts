1e6ad962c1fc61b5e1278aae04c5535a665e9ab2
LimbergDV <limbergdelvalle@gmail.com>
2025-02-06 15:19:04 -0600

    add: adding methods in repositories and methods in products.service

---
+++ src/app/ui/components/components.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductsListTableComponent } from './products-list-table/products-list-table.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProductsListTableComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ComponentsModule { }
---
+++ src/app/ui/components/products-list-table/products-list-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-list-table',
+  templateUrl: './products-list-table.component.html',
+  styleUrl: './products-list-table.component.css'
+})
+export class ProductsListTableComponent {
+
+}
